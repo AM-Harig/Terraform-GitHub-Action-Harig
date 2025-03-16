dd370bd89835707bbe7977ffeb59bf47c4c4a75d
AM-Harig <AM-Harig@outlook.com>
2025-03-16 19:07:50 -0400

    Assinf Terraform files

--- main.tf
+++ main.tf
@@ -47,3 +47,52 @@ resource "azurerm_subnet" "CR460-2025" {
   virtual_network_name = azurerm_virtual_network.CR460-2025.name
   address_prefixes     = ["10.0.1.0/24"]
 }
+# Adresse IP publique
+resource "azurerm_public_ip" "CR460-2025" {
+  name                = "CR460-2025-ip"
+  location            = azurerm_resource_group.CR460-2025.location
+  resource_group_name = azurerm_resource_group.CR460-2025.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+# Interface réseau
+resource "azurerm_network_interface" "CR460-2025" {
+  name                = "CR460-2025-nic"
+  location            = azurerm_resource_group.CR460-2025.location
+  resource_group_name = azurerm_resource_group.CR460-2025.name
+  ip_configuration {
+    name                          = "CR460-2025-ipconfig"
+    subnet_id                     = azurerm_subnet.CR460-2025.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.CR460-2025.id
+  }
+}
+# Machine virtuelle
+resource "azurerm_virtual_machine" "CR460-2025" {
+  name                  = "CR460-2025-vm"
+  location              = azurerm_resource_group.CR460-2025.location
+  resource_group_name   = azurerm_resource_group.CR460-2025.name
+  network_interface_ids = [azurerm_network_interface.CR460-2025.id]
+  vm_size               = "Standard_DS1_v2"
+
+  storage_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2022-datacenter-g2"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "CR460-2025-osdisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "CR460-2025-vm"
+    admin_username = "adminuser"
+    admin_password = "Password1234!"
+  }
+  os_profile_windows_config {
+    provision_vm_agent = true
+  }
+} # Fin de la machine virtuelle
